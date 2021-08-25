1e64d3a747f7c6536618a5e2440d8ecb570223de
xtsoulfiretx <alexi.lambrou@gmail.com>
2021-08-25 17:24:15 +0100

    added most of the feedback

--- myscript.js
+++ myscript.js
@@ -84,6 +84,113 @@ window.addEventListener('load', function () {
   
   for (var i = 1; i <= 6; i++) {
     _loop(i);
+  };
+
+  // The code blocks below will look through the input fields of the contact form and change icons accordingly,
+  // to let the user know if they have left any of the inputs empty.
+
+const EmailRegex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+const characterReg = /^\s*[a-zA-Z0-9,\s]+\s*$/;
+
+let validateString = function validateString(value) {
+  if (value.match(characterReg) && value.length > 0) {
+    return true;
+  } else {
+    return false;
   }
-  
-  ;
+}
+
+let validateEmail = function validateEmail(email) {
+  if (email.match(EmailRegex) && email.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+$("#email").on('input', function () {
+  let isValid = validateEmail($("#email").val());
+
+  if (isValid) {
+    $(".email-cross").removeClass("icon-visible");
+    $(".email-cross").addClass("icon-hidden");
+    $(".email-neutral").addClass("icon-hidden");
+    $(".email-tick").removeClass("icon-hidden");
+    $(".email-tick").addClass("icon-visible");
+
+  } else {
+    $(".email-tick").removeClass("icon-visible");
+    $(".email-tick").addClass("icon-hidden");
+    $(".email-neutral").addClass("icon-hidden");
+    $(".email-cross").removeClass("icon-hidden");
+    $(".email-cross").addClass("icon-visible");
+  }
+});
+
+$("#first-name-input").on('input', function () {
+  let isValid = validateString($("#first-name-input").val());
+
+  if (isValid) {
+    $(".fname-cross").addClass("icon-hidden");
+    $(".fname-cross").removeClass("icon-visible");
+    $(".fname-neutral").addClass("icon-hidden");
+    $(".fname-tick").removeClass("icon-hidden");
+    $(".fname-tick").addClass("icon-visible");
+  } else {
+    $(".fname-neutral").addClass("icon-hidden");
+    $(".fname-tick").addClass("icon-hidden");
+    $(".fname-tick").removeClass("icon-visible");
+    $(".fname-cross").removeClass("icon-hidden");
+    $(".fname-cross").addClass("icon-visible");
+  }
+});
+
+$("#last-name-input").on('input', function () {
+  let isValid = validateString($("#last-name-input").val());
+
+  if (isValid) {
+    $(".lname-cross").addClass("icon-hidden");
+    $(".lname-cross").removeClass("icon-visible");
+    $(".lname-neutral").addClass("icon-hidden");
+    $(".lname-tick").removeClass("icon-hidden");
+    $(".lname-tick").addClass("icon-visible");
+  } else {
+    $(".lname-neutral").addClass("icon-hidden");
+    $(".lname-tick").addClass("icon-hidden");
+    $(".lname-tick").removeClass("icon-visible");
+    $(".lname-cross").removeClass("icon-hidden");
+    $(".lname-cross").addClass("icon-visible");
+  }
+});
+
+$("#subject").on('input', function () {
+  let isValid = validateString($("#subject").val());
+
+  if (isValid) {
+    $(".subject-cross").addClass("icon-hidden");
+    $(".subject-cross").removeClass("icon-visible");
+    $(".subject-neutral").addClass("icon-hidden");
+    $(".subject-tick").removeClass("icon-hidden");
+    $(".subject-tick").addClass("icon-visible");
+  } else {
+    $(".subject-neutral").addClass("icon-hidden");
+    $(".subject-tick").addClass("icon-hidden");
+    $(".subject-tick").removeClass("icon-visible");
+    $(".subject-cross").removeClass("icon-hidden");
+    $(".subject-cross").addClass("icon-visible");
+  }
+});
+
+$("#message").on('input', function () {
+  let isValid = validateString($("#message").val());
+
+  if (isValid) {
+    $("#message-warning").removeClass(".icon-visible");
+    $("#message-warning").addClass(".icon-hidden");
+  } else {
+    $("#message-warning").addClass(".icon-visible");
+    $("#message-warning").removeClass(".icon-hidden");
+  }
+});
+
+// End of input validation.
