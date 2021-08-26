29761e27b2a42c2b588d988001b4b2c652f7ca44
xtsoulfiretx <alexi.lambrou@gmail.com>
2021-08-26 16:09:42 +0100

    first round of bug fixing

--- myscript.js
+++ myscript.js
@@ -1,70 +1,71 @@
+"use strict";
+
 // Typing effect for header H1 element //
 window.addEventListener('load', function () {
-    function createTypewriter(text, id) {
-      var i = 0;
-      var speed = 90;
-  
-      function typeWriter(text, id) {
-        if (i < text.length) {
-          document.getElementById(id).textContent += text.charAt(i);
-          i++;
-          setTimeout(function () {
-            typeWriter(text, id);
-          }, speed);
-        }
+  function createTypewriter(text, id) {
+    var i = 0;
+    var speed = 90;
+
+    function typeWriter(text, id) {
+      if (i < text.length) {
+        document.getElementById(id).textContent += text.charAt(i);
+        i++;
+        setTimeout(function () {
+          typeWriter(text, id);
+        }, speed);
       }
-  
-      typeWriter(text, id);
     }
-  
-    $(".links-container").slideUp(0000);
-    $(".explanation-text").slideUp(0000);
-    createTypewriter("My Name is Alexi Lambrou", "greetings");
-    setTimeout(function () {
-      createTypewriter("I'm a Web Developer!", "banner-tagline");
-    }, 3000);
-  }); // Hambuger click effect //
-  
-  $(".hamburger").click(function () {
-    $(".hamburger").toggleClass("is-active");
-    $(".links-container").slideToggle();
-  }); // Code explanation reveal //
-  
-  $("#whats-this1").click(function () {
-    $("#whats-this1").toggleClass("code-shown");
-    $("#explanation1").slideToggle();
+
+    typeWriter(text, id);
+  }
+
+  $(".links-container").slideUp(0);
+  $(".explanation-text").slideUp(0);
+  createTypewriter("My Name is Alexi Lambrou", "greetings");
+  setTimeout(function () {
+    createTypewriter("I'm a Web Developer!", "banner-tagline");
+  }, 3000);
+}); // Hambuger click effect //
+
+$(".hamburger").click(function () {
+  $(".hamburger").toggleClass("is-active");
+  $(".links-container").slideToggle();
+}); // Code explanation reveal //
+
+$("#whats-this1").click(function () {
+  $("#whats-this1").toggleClass("code-shown");
+  $("#explanation1").slideToggle();
+});
+$("#whats-this2").click(function () {
+  $("#whats-this2").toggleClass("code-shown");
+  $("#explanation2").slideToggle();
+}); // Projects Hidden boxes //
+
+var _loop = function _loop(i) {
+  $("#project-".concat(i)).click(function () {
+    $("#hb".concat(i)).slideToggle();
   });
-  
-  $("#whats-this2").click(function () {
-    $("#whats-this2").toggleClass("code-shown");
-    $("#explanation2").slideToggle();
-  });// Projects Hidden boxes //
-  
-  var _loop = function _loop(i) {
-    $("#project-".concat(i)).click(function () {
-        $("#hb".concat(i)).slideToggle();
-    });
-  };
-  
-  for (var i = 1; i <= 6; i++) {
-    _loop(i);
-  };
+};
 
-  // The code blocks below will look through the input fields of the contact form and change icons accordingly,
-  // to let the user know if they have left any of the inputs empty.
+for (var i = 1; i <= 6; i++) {
+  _loop(i);
+}
 
-const EmailRegex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-const characterReg = /^\s*[a-zA-Z0-9,\s]+\s*$/;
+; // The code blocks below will look through the input fields of the contact form and change icons accordingly,
+// to let the user know if they have left any of the inputs empty.
 
-let validateString = function validateString(value) {
+var EmailRegex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+var characterReg = /^\s*[a-zA-Z0-9,\s]+\s*$/;
+
+var validateString = function validateString(value) {
   if (value.match(characterReg) && value.length > 0) {
     return true;
   } else {
     return false;
   }
-}
+};
 
-let validateEmail = function validateEmail(email) {
+var validateEmail = function validateEmail(email) {
   if (email.match(EmailRegex) && email.length > 0) {
     return true;
   } else {
@@ -73,7 +74,7 @@ let validateEmail = function validateEmail(email) {
 };
 
 $("#email").on('input', function () {
-  let isValid = validateEmail($("#email").val());
+  var isValid = validateEmail($("#email").val());
 
   if (isValid) {
     $(".email-cross").removeClass("icon-visible");
@@ -81,7 +82,6 @@ $("#email").on('input', function () {
     $(".email-neutral").addClass("icon-hidden");
     $(".email-tick").removeClass("icon-hidden");
     $(".email-tick").addClass("icon-visible");
-
   } else {
     $(".email-tick").removeClass("icon-visible");
     $(".email-tick").addClass("icon-hidden");
@@ -90,9 +90,8 @@ $("#email").on('input', function () {
     $(".email-cross").addClass("icon-visible");
   }
 });
-
 $("#first-name-input").on('input', function () {
-  let isValid = validateString($("#first-name-input").val());
+  var isValid = validateString($("#first-name-input").val());
 
   if (isValid) {
     $(".fname-cross").addClass("icon-hidden");
@@ -108,9 +107,8 @@ $("#first-name-input").on('input', function () {
     $(".fname-cross").addClass("icon-visible");
   }
 });
-
 $("#last-name-input").on('input', function () {
-  let isValid = validateString($("#last-name-input").val());
+  var isValid = validateString($("#last-name-input").val());
 
   if (isValid) {
     $(".lname-cross").addClass("icon-hidden");
@@ -126,9 +124,8 @@ $("#last-name-input").on('input', function () {
     $(".lname-cross").addClass("icon-visible");
   }
 });
-
 $("#subject").on('input', function () {
-  let isValid = validateString($("#subject").val());
+  var isValid = validateString($("#subject").val());
 
   if (isValid) {
     $(".subject-cross").addClass("icon-hidden");
@@ -143,12 +140,10 @@ $("#subject").on('input', function () {
     $(".subject-cross").removeClass("icon-hidden");
     $(".subject-cross").addClass("icon-visible");
   }
-});
-
-// This block will look through the message field for special characters and disable the submit button if any are found.
+}); // This block will look through the message field for special characters and disable the submit button if any are found.
 
 $("#message").on('input', function () {
-  let isValid = validateString($("#message").val());
+  var isValid = validateString($("#message").val());
 
   if (isValid) {
     $("#message-warning").removeClass("icon-visible");
@@ -161,6 +156,4 @@ $("#message").on('input', function () {
     $("#btnSubmit").addClass("btnoff");
     $("#btnSubmit").attr("disabled", true);
   }
-});
-
-// End of input validation.
+}); // End of input validation.
--- uncompiledjs/myscript.js
+++ uncompiledjs/myscript.js
@@ -1,91 +1,166 @@
 // Typing effect for header H1 element //
+window.addEventListener('load', function () {
+  function createTypewriter(text, id) {
+    var i = 0;
+    var speed = 90;
 
-window.addEventListener('load', function() {
-    
-    function createTypewriter(text,id){
-        var i = 0;
-        var speed = 90;
-        
-        function typeWriter(text,id) {
-            if (i < text.length) {
-                document.getElementById(id).textContent += text.charAt(i);
-                i++;
-                setTimeout(function() {typeWriter(text,id)}, speed);
-            }
-        }
-        typeWriter(text,id);
-    }
-
-    if (window.innerWidth >= 1024) {
-
-    console.log ("test success");
-    $("#hb1").slideUp(0000);
-    $("#hb2").slideUp(0000);
-    $("#hb3").slideUp(0000);
-    $("#hb4").slideUp(0000);
-    $("#hb5").slideUp(0000);
-    $("#hb6").slideUp(0000);
-    } else {
-        $("#hb1").slideDown(0000);
-        $("#hb2").slideDown(0000);
-        $("#hb3").slideDown(0000);
-        $("#hb4").slideDown(0000);
-        $("#hb5").slideDown(0000);
-        $("#hb6").slideDown(0000);
-        console.log ("test fail");
-    }
-
-
-    $(".links-container").slideUp(0000);
-    $(".explanation-text").slideUp(0000);
-    createTypewriter("My Name is Alexi Lambrou", "greetings");
-    setTimeout(function (){createTypewriter("I'm a Web Developer!", "banner-tagline")},3000);
-  });
-
-  $(window).resize(function() {
-      if (window.innerWidth >= 1024) {
-            $("#hb1").slideUp(0000);
-            $("#hb2").slideUp(0000);
-            $("#hb3").slideUp(0000);
-            $("#hb4").slideUp(0000);
-            $("#hb5").slideUp(0000);
-            $("#hb6").slideUp(0000);
-        } else {
-            $("#hb1").slideDown(0000);
-            $("#hb2").slideDown(0000);
-            $("#hb3").slideDown(0000);
-            $("#hb4").slideDown(0000);
-            $("#hb5").slideDown(0000);
-            $("#hb6").slideDown(0000);
-        }
-  })
-
-
-  // Hambuger click effect //
-
-  $(".hamburger").click(function(){
-      $(".hamburger").toggleClass("is-active");
-      $(".links-container").slideToggle();
-  });
-
-  // Code explanation reveal //
-
-  $(".whats-this1").click(function(){
-    $(".whats-this1").toggleClass("code-shown");
-    $(".explanation1").slideToggle();
-  });
-
-
-  // Projects Hidden boxes //
-
-  for (let i = 1; i <= 6; i++) {
-    $(`#project-${i}`).hover(function () {
-      if (window.innerWidth >= 1024) {
-        $(`#hb${i}`).slideToggle();
-        console.log("this should be working")
-      } else {
-        console.log("fails")
+    function typeWriter(text, id) {
+      if (i < text.length) {
+        document.getElementById(id).textContent += text.charAt(i);
+        i++;
+        setTimeout(function () {
+          typeWriter(text, id);
+        }, speed);
       }
-  })
-  };
+    }
 
+    typeWriter(text, id);
+  }
+
+  $(".links-container").slideUp(0o000);
+  $(".explanation-text").slideUp(0o000);
+  createTypewriter("My Name is Alexi Lambrou", "greetings");
+  setTimeout(function () {
+    createTypewriter("I'm a Web Developer!", "banner-tagline");
+  }, 3000);
+}); // Hambuger click effect //
+
+$(".hamburger").click(function () {
+  $(".hamburger").toggleClass("is-active");
+  $(".links-container").slideToggle();
+}); // Code explanation reveal //
+
+$("#whats-this1").click(function () {
+  $("#whats-this1").toggleClass("code-shown");
+  $("#explanation1").slideToggle();
+});
+
+$("#whats-this2").click(function () {
+  $("#whats-this2").toggleClass("code-shown");
+  $("#explanation2").slideToggle();
+});// Projects Hidden boxes //
+
+var _loop = function _loop(i) {
+  $("#project-".concat(i)).click(function () {
+      $("#hb".concat(i)).slideToggle();
+  });
+};
+
+for (var i = 1; i <= 6; i++) {
+  _loop(i);
+};
+
+// The code blocks below will look through the input fields of the contact form and change icons accordingly,
+// to let the user know if they have left any of the inputs empty.
+
+const EmailRegex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+const characterReg = /^\s*[a-zA-Z0-9,\s]+\s*$/;
+
+let validateString = function validateString(value) {
+if (value.match(characterReg) && value.length > 0) {
+  return true;
+} else {
+  return false;
+}
+}
+
+let validateEmail = function validateEmail(email) {
+if (email.match(EmailRegex) && email.length > 0) {
+  return true;
+} else {
+  return false;
+}
+};
+
+$("#email").on('input', function () {
+let isValid = validateEmail($("#email").val());
+
+if (isValid) {
+  $(".email-cross").removeClass("icon-visible");
+  $(".email-cross").addClass("icon-hidden");
+  $(".email-neutral").addClass("icon-hidden");
+  $(".email-tick").removeClass("icon-hidden");
+  $(".email-tick").addClass("icon-visible");
+
+} else {
+  $(".email-tick").removeClass("icon-visible");
+  $(".email-tick").addClass("icon-hidden");
+  $(".email-neutral").addClass("icon-hidden");
+  $(".email-cross").removeClass("icon-hidden");
+  $(".email-cross").addClass("icon-visible");
+}
+});
+
+$("#first-name-input").on('input', function () {
+let isValid = validateString($("#first-name-input").val());
+
+if (isValid) {
+  $(".fname-cross").addClass("icon-hidden");
+  $(".fname-cross").removeClass("icon-visible");
+  $(".fname-neutral").addClass("icon-hidden");
+  $(".fname-tick").removeClass("icon-hidden");
+  $(".fname-tick").addClass("icon-visible");
+} else {
+  $(".fname-neutral").addClass("icon-hidden");
+  $(".fname-tick").addClass("icon-hidden");
+  $(".fname-tick").removeClass("icon-visible");
+  $(".fname-cross").removeClass("icon-hidden");
+  $(".fname-cross").addClass("icon-visible");
+}
+});
+
+$("#last-name-input").on('input', function () {
+let isValid = validateString($("#last-name-input").val());
+
+if (isValid) {
+  $(".lname-cross").addClass("icon-hidden");
+  $(".lname-cross").removeClass("icon-visible");
+  $(".lname-neutral").addClass("icon-hidden");
+  $(".lname-tick").removeClass("icon-hidden");
+  $(".lname-tick").addClass("icon-visible");
+} else {
+  $(".lname-neutral").addClass("icon-hidden");
+  $(".lname-tick").addClass("icon-hidden");
+  $(".lname-tick").removeClass("icon-visible");
+  $(".lname-cross").removeClass("icon-hidden");
+  $(".lname-cross").addClass("icon-visible");
+}
+});
+
+$("#subject").on('input', function () {
+let isValid = validateString($("#subject").val());
+
+if (isValid) {
+  $(".subject-cross").addClass("icon-hidden");
+  $(".subject-cross").removeClass("icon-visible");
+  $(".subject-neutral").addClass("icon-hidden");
+  $(".subject-tick").removeClass("icon-hidden");
+  $(".subject-tick").addClass("icon-visible");
+} else {
+  $(".subject-neutral").addClass("icon-hidden");
+  $(".subject-tick").addClass("icon-hidden");
+  $(".subject-tick").removeClass("icon-visible");
+  $(".subject-cross").removeClass("icon-hidden");
+  $(".subject-cross").addClass("icon-visible");
+}
+});
+
+// This block will look through the message field for special characters and disable the submit button if any are found.
+
+$("#message").on('input', function () {
+let isValid = validateString($("#message").val());
+
+if (isValid) {
+  $("#message-warning").removeClass("icon-visible");
+  $("#message-warning").addClass("icon-hidden");
+  $("#btnSubmit").removeClass("btnoff");
+  $("#btnSubmit").attr("disabled", false);
+} else {
+  $("#message-warning").addClass("icon-visible");
+  $("#message-warning").removeClass("icon-hidden");
+  $("#btnSubmit").addClass("btnoff");
+  $("#btnSubmit").attr("disabled", true);
+}
+});
+
+// End of input validation.
